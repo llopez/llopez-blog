fedae8764434510a1fe9b75dc1c5186413f3a01c
luis lopez <luigibyte@gmail.com>
2010-06-26 17:07:39 -0300

    new method tag generator

--- _helpers.rb
+++ _helpers.rb
@@ -2,24 +2,21 @@ require 'haml-coderay'
 DOMAIN_URL = "http://llopez.heroku.com"  
 FEED_URL = "/feed/atom.xml"
 module Helpers
-  #def render(options = {}) 
-  #end
-  def header
-    ['Curriculum', 'Proyectos', 'Fotos']
-    haml_tag :div, "este es el header"
-  end
+  
   def image(image_path, options={})
     options.merge!({:src => "/images/#{image_path}"})
     haml_tag :img, options
   end
   
-  def tags(page)
-    page.tags.join(",")
+  def tags(page_or_site)    
+    tags = page_or_site.tags
+    tags.is_a?(Hash) ? tags.keys.join(",") : tags.join(",")
   end
   
   def title(page)
-  ">_ /posts/" + page.categories.join("/") + "/" + page.title.gsub(" ","_")
+    ">_ /posts/" + page.categories.join("/") + "/" + page.title.gsub(" ","_")
   end
+  
   def categories(post)
     post.categories.collect do |category|
       haml_tag :span,{:class => category} do
@@ -29,4 +26,5 @@ module Helpers
       end
     end
   end
+  
 end
