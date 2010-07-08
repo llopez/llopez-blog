3a5b51238a7228f7fe155bf5a150311ea783c594
luis lopez <luigibyte@gmail.com>
2010-07-08 18:35:14 -0300

    improving the generation of tags

--- _helpers.rb
+++ _helpers.rb
@@ -1,22 +1,19 @@
 require 'haml-coderay'
-DOMAIN_URL = "http://llopez.heroku.com"  
+DOMAIN_URL = "http://llopezblog.com.ar"  
 FEED_URL = "/feed/atom.xml"
 module Helpers
-  
   def image(image_path, options={})
     options.merge!({:src => "/images/#{image_path}"})
     haml_tag :img, options
   end
-  
-  def tags(page_or_site)    
-    tags = page_or_site.tags
-    tags.is_a?(Hash) ? tags.keys.join(",") : tags.join(",")
+  def meta_tags(page, site = nil)
+    tags = page.tags
+    return site.categories.keys.join(',') unless tags
+    tags.is_a?(Hash) ? tags.keys.join(",") : tags.join(",")  
   end
-  
   def title(page)
     ">_ /posts/" + page.categories.join("/") + "/" + page.title.gsub(" ","_")
   end
-  
   def categories(post)
     post.categories.collect do |category|
       haml_tag :span,{:class => category} do
@@ -26,5 +23,4 @@ module Helpers
       end
     end
   end
-  
 end
